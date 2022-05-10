d7780e5d0e3b99573abb36b5beed9e144b195d1b
poojaEminence <poojamaurya.eminence@gmail.com>
2022-05-10 10:02:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import Registration from "./components/home/Registration";
 import User from "./components/home/User";
 import AllData from "./components/home/AllData";
+import View from "./components/home/View";
 //import Data from "./components/home/Data";
 //import Home from "./components/home/Home";
 //import GetData from "./components/home/GetData";
@@ -19,6 +20,7 @@ function App() {
         {/* <Route path="/" element={<Registration />}></Route> */}
         <Route path="/" element={<User />}></Route>
         <Route path="/alldata" element={<AllData />}></Route>
+        <Route path="/viewdata" element={<View />}></Route>
         {/* <Route path="/data" element={<Data />}></Route> */}
       </Routes>
     </BrowserRouter>
